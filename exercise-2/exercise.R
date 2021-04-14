8332bde95c728a2e6c96df0d621e2973a7a468a2
hemmys <clauked@outlook.com>
2021-04-13 20:19:45 -0700

    code

--- exercise-2/exercise.R
+++ exercise-2/exercise.R
@@ -11,6 +11,21 @@
 allEven <- function(x) {
   if(all(x %% 2 == 0)) {
     cat("all even")
+  }
+}
+
+## Pass a vector to test if it works correctly
+
+allEven(c(2, 4, 6, 8))
+
+## Write a function that takes a vector of numbers as argument
+## and tells if among all the numbers there is at least one
+## odd number
+## Hint: check out 'any' function
+
+oneOdd <- function(x) {
+  if(all(x %% 2 == 0)) {
+    cat("all even")
   } else {
     cat("at least one odd")
     }
@@ -18,23 +33,15 @@ allEven <- function(x) {
 
 ## Pass a vector to test if it works correctly
 
-allEven(c(1, 2, 3, 5, 6, 8))
-
-## Write a function that takes a vector of numbers as argument
-## and tells if among all the numbers there is at least one
-## odd number
-## Hint: check out 'any' function
-
-
-## Pass a vector to test if it works correctly
-
+oneOdd(c(1, 2, 3, 5, 6, 8))
 
 ### ---------- vector length ----------
 
 # Write a function `CompareLength` that takes in 2 vectors, and returns the sentence:
 # "The difference in lengths is N"
 
-
+CompareLength <- 
+  
 ## Pass two vectors of different length to your `CompareLength` function
 ## to check if it works as intended 
 
