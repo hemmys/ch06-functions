890864d045ff3abdd52f536406bd76f6eea561aa
hemmys <clauked@outlook.com>
2021-04-13 22:17:05 -0700

    finished code for exercise 2

--- exercise-2/exercise.R
+++ exercise-2/exercise.R
@@ -40,17 +40,34 @@ oneOdd(c(1, 2, 3, 5, 6, 8))
 # Write a function `CompareLength` that takes in 2 vectors, and returns the sentence:
 # "The difference in lengths is N"
 
-CompareLength <- 
+CompareLength <- function(v1, v2) {
+  cat("The difference in lengths is", abs(length(v1) - length(v2)))
+}
   
 ## Pass two vectors of different length to your `CompareLength` function
 ## to check if it works as intended 
 
+CompareLength(c("chicken nuggets", "hot dog", "pizza"), c("bologna"))
+
+CompareLength(c("chocolate", "chip", "cookie"), c("chicken", "wing", "chicken", "wing", "hot dog", "and", "bologna"))
 
 # Write a function `DescribeDifference` that will return one of the following statements:
 # "Your first vector is longer by N elements"
 # "Your second vector is longer by N elements"
 
+DescribeDifference <- function(a, b) {
+  if(length(a) > length(b)) {
+    cat("Your first vector is longer by", abs(length(a) - length(b)), "elements")
+  } else {
+    cat("Your second vector is longer by", abs(length(b) - length(a)), "elements")
+  }
+}
 
 ## Pass two vectors to your `DescribeDifference` function
 ## check if it works correctly
+
+DescribeDifference(c("turquoise", "lavender", "olive green"), c("violet", "pink", "blue", "green"))
+
+DescribeDifference(c("magenta", "brown", "white"), c("black", "orange", "peach", "sunflower", "daisy", "strawberry"))
+
 
